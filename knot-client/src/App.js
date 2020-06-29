f402aeb46c7133b3abb9c659523e630a95c9979f
NGAJISUBUH <mufcajo1@gmail.com>
2020-06-29 23:05:50 +0700

    knot app

---
+++ knot-client/src/App.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+import { ThemeProvider as MuiThemeProvider } from "@material-ui/core/styles";
+import themeFile from "./util/theme";
+import JwtDecode from "jwt-decode";
+
+// Redux
+import { Provider } from "react-redux";
+import store from "./redux/store";
+import { SET_AUTHENTICATED } from "./redux/types";
+import { logoutUser, getUserData } from "./redux/actions/userActions";
+
+// components
+import Navbar from "./components/layout/Navbar";
+import AuthRoute from "./util/AuthRoute";
+
+// pages
+import home from "./pages/home";
+import login from "./pages/login";
+import signup from "./pages/signup";
+import user from "./pages/user";
+import axios from "axios";
+
+const theme = createMuiTheme(themeFile);
+
+const token = localStorage.FBIdToken;
+if (token) {
+  const decodedToken = JwtDecode(token);
+  if (decodedToken.exp * 1000 < Date.now()) {
+    window.location.href = "/login";
+    store.dispatch(logoutUser());
+  } else {
+    store.dispatch({ type: SET_AUTHENTICATED });
+    axios.defaults.headers.common["Authorization"] = token;
+    store.dispatch(getUserData());
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <MuiThemeProvider theme={theme}>
+        <Provider store={store}>
+          <Router>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={home} />
+                <AuthRoute exact path="/login" component={login} />
+                <AuthRoute exact path="/signup" component={signup} />
+                <Route exact path="/users/:handle" component={user} />
+                <Route
+                  exact
+                  path="/users/:handle/scream/:screamId"
+                  component={user}
+                />
+              </Switch>
+            </div>
+          </Router>
+        </Provider>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default App;
---
+++ knot-functions/functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apiKey: "AIzaSyDr5ldjNbu7XosHZF5QLS_4R8lvTi8kSmE",
+  authDomain: "knot-39514.firebaseapp.com",
+  databaseURL: "https://knot-39514.firebaseio.com",
+  projectId: "knot-39514",
+  storageBucket: "knot-39514.appspot.com",
+  messagingSenderId: "559281264214",
+  appId: "1:559281264214:web:db1d8eaa333d7fce6a0df7",
+  measurementId: "G-Q4X6701KZW",
+};
